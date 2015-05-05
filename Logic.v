139d97fc1fb32b2eb5b94c75cdef68fca5d9d87b
katherineye <kye@princeton.edu>
2015-05-05 03:32:23 -0400

    read pfenning chapters 1-3, write down my understanding of it (still don't see how many of the left rules correspond to elim rules), write down list of connectives (what about quantifiers and bangs/classical ones??), type/prop level encoding, and sequents

--- Logic.v
+++ Logic.v
@@ -19,16 +19,76 @@ Goal for Wednesday night:
 - auxiliary lemmas
 - encode one example (blocks world, tower of hanoi, ?)
 - with manual proof
-
-
 *)
 
-Inductive ILinProp : Set :=
-  | Implies : ILinProp -> ILinProp -> ILinProp
-  | One : ILinProp
-  | Plus : ILinProp -> ILinProp -> ILinProp
-  | Times : ILinProp -> ILinProp -> ILinProp
-  | Top : ILinProp
-  | With : ILinProp -> ILinProp -> ILinProp
-  | Zero : ILinProp.
+(* Sequent calculus version:
+
+Split judgment (A is true) into two judgments: A is a resource; A is a goal
+Resources: factories (unrestricted); normal (linear)
+
+Rules: right rule (goal) = intro rule; (??) -- this corresponds directly!
+       left rule (resource) = elim rule (oh... stuff you have in context)
+         -- 
+       so like inversion? does that need to be explicitly encoded?
+
+
+In the rules, why doesn't Pfenning label with whether they are factories or normal (restricted)???
+
+Two cut rules?? Some stuff about coercion?? *)
+
+(* 
+- ILL props
+- ILL connectives
+- Notations for them
+- ILL rules (sequents)
+- Notations for them
+*)
+
+(*
+ILL connectives: note, some are binary, some are unary, some are nullary
+
+(- Atomic proposition -- only in ILL paper -- with type Vars.t = N)
+
+* Multiplicative
+- Linear implication -o
+- Simultaneous conjunction ((X)) or ** (times)
+- Multiplicative truth: 1
+
+* Additive
+- Alternative conjunction (&) (with)
+- Plus (+)
+- True
+- 0
+
+* Quantifiers?
+- forall
+- exists
+
+* Exponentials?
+- subset
+- bang
+
+so we don't need to model classical props? what about factories? shouldn't we distinguish between linear and classical/unlimited props? ILinProp = Resource... could encode using bang?
+
+(ILL props just have type ILinProp
+- Name? How to represent the prop A?
+- it DOESN'T MATTER what value inhabits type ILinProp! for example, we have "empty : ILinProp"
+)
+ *)
+
+(* ILL rules:
+
+Gamma |- A, B...N not possible -- only
+Gamma |- A? (where A = A (+) B... if necessary)
+
+so Gamma |- A = LinCons (or Turnstile) (Gamma/Resources : list ILinProp) (A : ILinProp) : Prop
+
+so, how can you construct/prove? a (single-consequent) sequent in ILL?
+well, if you want to construct/prove something of form (d1, d2, A -o B) |- C,
+  you can do it by constructing/proving two things
+  one of form (D1 |- A), another of form (d2 ^ B |- C)...
+
+and so on for each rule!
+
+*)
 
